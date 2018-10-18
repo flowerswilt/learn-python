bf14b6301871da6da085334a7685dd6956f707c1
zhq <flowerswilt@gmail.com>
2018-10-18 21:58:00 +0800

    完善生成器

--- 核心/04.生成器.py
+++ 核心/04.生成器.py
@@ -8,7 +8,9 @@ print(a_gen)
 
 # 使用内置函数next取出生成器中的值
 print(next(a_gen))
-# 当取出最后一个生成器的值后，再次调用next会得到一个异常
+print(dir(a_gen)) # 内置函数next()其实调用的是生成器对象的__next__()方法
+
+# 当取出最后一个生成器的值后，再次调用next会得到一个异常 StopIteration
 # for循环可以优雅的处理边界情况
 for a in a_gen:
     print(a)
@@ -27,4 +29,19 @@ f_gen = createNum() # 返回一个生成器
 
 for x in f_gen: # 用for循环来处理生成器取值
     print(x)
-    
+
+# 可以通过生成器的send()方法与生成器对象交互
+def sendTest():
+    i = 0
+    while i < 5:
+        temp = yield i
+        print(temp)
+        i += 1
+
+send_gen = sendTest()
+print(next(send_gen)) # 0
+print(send_gen.send('temp接收这个信息')) # 输出: temp接收这个信息 1
+
+# 需要注意的是，一个刚刚启动的生成器对象，在第一次next取值前，不应该send()消息，否则会得到一个TypeError异常
+# 或者对于一个刚启动的生成器对象，使用send(None)方法
+
