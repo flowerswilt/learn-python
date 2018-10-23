6678c6aac8ecb9320427912f750009d5cfd2f0c1
zhq <flowerswilt@gmail.com>
2018-10-23 09:22:33 +0800

    完成小案例

--- 系统编程/05.多进程拷贝文件.py
+++ 系统编程/05.多进程拷贝文件.py
@@ -1,25 +1,26 @@
 from multiprocessing import Pool
 import os
 
-def copyTask(dir_name, file_name):
+def copyTask(dir_name, new_dir_name, file_name):
     "拷贝文件函数"
     fr = open(dir_name + '/' + file_name, mode='rb') # 读取文件句柄
-    # fw = open(dir_name + '-back' + '/' + file_name, mode='wb')  # 写入文件句柄
+    fw = open(new_dir_name + '/' + file_name, mode='wb')  # 写入文件句柄
     content = fr.read()
-    print(content)
-    # fw.write(content)
+    fw.write(content)
     # print('拷贝开始')
     fr.close()
-    # fw.close()
+    fw.close()
 
 def main():
     "主函数"
     # 接收用户输入的文件夹名称
     dir_name = input('请输入文件夹名称>>>: ')
     file_names = os.listdir(dir_name) # 拿到文件夹内的全部文件名称
+    new_dir_name = dir_name + '-back'
+    os.mkdir(new_dir_name)
     pool = Pool(10) # 开启10个进程
     for file_name in file_names:
-        pool.apply_async(copyTask, args=(dir_name, file_name))
+        pool.apply_async(copyTask, args=(dir_name, new_dir_name, file_name))
 
     pool.close()
     pool.join()
